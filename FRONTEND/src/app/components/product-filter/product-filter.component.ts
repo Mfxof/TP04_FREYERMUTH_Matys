3afcf79dc859e8f7c49f22b677df38e1624a873f
Mfxof <matysfreyermuth@gmail.com>
2024-10-26 13:52:01 +0200

    Rajout du tri / .json

--- FRONTEND/src/app/components/product-filter/product-filter.component.ts
+++ FRONTEND/src/app/components/product-filter/product-filter.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-product-filter',
   standalone: true,
-  imports: [],
   templateUrl: './product-filter.component.html',
-  styleUrl: './product-filter.component.css'
+  styleUrls: ['./product-filter.component.css']
 })
 export class ProductFilterComponent {
+  @Output() filterChanged = new EventEmitter<string[]>();
+  @Output() sortChanged = new EventEmitter<string>();
+  @Output() tagSorted = new EventEmitter<string>();
+
+  tags = ['Animal', 'Véhicule', 'Nourriture'];
+  selectedTags: string[] = [];
+  sortOrder: string = 'asc';
+
+  toggleTag(tag: string) {
+    const index = this.selectedTags.indexOf(tag);
+    if (index === -1) {
+      this.selectedTags.push(tag);
+    } else {
+      this.selectedTags.splice(index, 1);
+    }
+    this.filterChanged.emit(this.selectedTags);
+  }
+
+  changeSortOrder(order: string) {
+    this.sortOrder = order;
+    this.sortChanged.emit(this.sortOrder);
+  }
+
+  changeSortByTag(event: Event) {
+    const target = event.target as HTMLSelectElement;
+    const tag = target.value;
+    this.tagSorted.emit(tag);
+}
 
 }
