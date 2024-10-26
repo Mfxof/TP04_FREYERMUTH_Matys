097e790796eb3256e4fbc285772483d3472b4970
Mfxof <matysfreyermuth@gmail.com>
2024-10-26 13:23:07 +0200

    Affichage de la list d'objets

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductFilterComponent } from './components/product-filter/product-filter.component';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ProductFilterComponent, ProductListComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
