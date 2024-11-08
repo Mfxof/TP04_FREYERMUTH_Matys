5bc2dbca16b6c582a73c9587a194432b00edc6f0
Mfxof <matysfreyermuth@gmail.com>
2024-11-08 14:17:15 +0100

    Rajout d'un index

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/side/header/header.component';
 import { FooterComponent } from './components/side/footer/footer.component';
 import { CarouselComponent } from './components/side/carousel/carousel.component';
+import { IndexBodyComponent } from './components/body/index/index-body/index-body.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, CarouselComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, CarouselComponent, IndexBodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ FRONTEND/src/app/components/body/index/index-body/index-body.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-body',
+  standalone: true,
+  imports: [],
+  templateUrl: './index-body.component.html',
+  styleUrl: './index-body.component.css'
+})
+export class IndexBodyComponent {
+
+}
