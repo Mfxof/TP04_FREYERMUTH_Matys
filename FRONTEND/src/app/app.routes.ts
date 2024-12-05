46ad7c309f334f624f94ffc8baa84578ff63ff4c
Mfxof <matysfreyermuth@gmail.com>
2024-12-05 17:46:17 +0100

    Suite du cart / achat

--- FRONTEND/src/app/app.routes.ts
+++ FRONTEND/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { CardsformComponent } from './components/body/cards/cards-form/cards-form.component';
 import { ProductListComponent } from './components/body/shop/product-list/product-list.component';
 import { CarouselComponent } from './components/side/carousel/carousel.component';
+import { CartComponent } from './components/body/shop/cart/cart.component';
 
 export const routes: Routes = [
   { path: 'boutique', component: ProductListComponent },
   { path: 'index', component: ProductListComponent },
   { path: 'paiement', component: CardsformComponent },
+  { path: 'cart/:id', component: CartComponent },
   {
     path: '**',
     component: CarouselComponent,
